8779b77dd42d44e9b0a3de27e13151e2845a31de
Viktoria <doduchenko29@gmail.com>
2021-07-05 18:01:58 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { renderTasks } from './list/render.js';
+import { initTodoListHandlers } from './list/todoList.js';
+import { getTasksList } from './list/tasksGateway.js';
+import { setItem } from './list/storage.js';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then((tasksList) => {
+    setItem('tasksList', tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
---
+++ src/list/todoList.js
@@ -0,0 +1,11 @@
+import { onCreateTask } from './createTask.js';
+import { onToggleTask } from './updateTask.js';
+
+export const initTodoListHandlers = () => {
+  const buttonElem = document.querySelector('.btn');
+  buttonElem.addEventListener('click', onCreateTask);
+
+  const listElem = document.querySelector('.list');
+  listElem.addEventListener('click', onListClick);
+  listElem.addEventListener('click', onToggleTask);
+};
